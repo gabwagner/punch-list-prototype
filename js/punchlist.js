f7875125ec1137b9e95edd6deaf0a0ae40d7cc3c
Gabriel Wagner <gabwagner@gmail.com>
2019-02-12 10:11:52 -0300

    Adding "Add Behaviour"

--- js/punchlist.js
+++ js/punchlist.js
@@ -50,6 +50,7 @@
     this._defaults = defaults;
     this._name = punchList;
     this._index = 0;
+    this._adding = false;
     this.init();
   }          
 
@@ -68,15 +69,58 @@
   }
   
   PunchList.prototype.addItem = function() {
-    this._index++;
-    var punchlist_items = $(this.element).find('#punchlist-items');
-    var new_item_html = $.map({ [this._index]:{index:false, item:'',comments:[]}},this.options.punchListItemTemplate).join('');
-    var newItem = $(new_item_html).appendTo(punchlist_items);
-    /*
-    var newItem = $([],$(punchlist_items)[0].ownerDocument);
-    
-    punchlist_items.append(newItem);
-    */
+    if(!this._adding) {
+      this._adding = true;
+      this._index++;
+      var punchlist_items = $(this.element).find('#punchlist-items');
+      var new_item_html = $.map({ [this._index]:{index:false, item:'',comments:[]}},this.options.punchListItemTemplate).join('');
+      var newItem = $(new_item_html).appendTo(punchlist_items);
+      
+      newItem.find('.punchlist-item-tags').remove();
+      
+      var toAppendInput = newItem.find('.punchlist-item-label-text-data');
+      
+      var input = document.createElement('input');
+      input.type = 'text';
+      input.id = 'punch-item' + this._index;
+      
+      var input = $(input).appendTo(toAppendInput);
+      
+      input.focus();
+      
+      var self = this;
+      
+      input.enterKey(function(){
+        $(this).trigger('enterEvent');
+      });
+            
+      input.on('blur enterEvent',function(){
+        var todoTitle = input.val();
+        var todoTitleLength = todoTitle.length;
+        if (todoTitleLength > 0) {
+          // TODO: Add new item handler
+          $(this).parent().html(todoTitle);
+          newItem.find('.fa-times-circle').click( function(){
+            self.removeItem(newItem);
+          });
+        } else {
+          newItem.remove();
+        }
+        self._adding = false;
+      });
+    }
+  }
+
+  PunchList.prototype.removeItem = function(item) {
+      // TODO: Add item remove handler
+      item.animate({
+        left:"-30%",
+        height:0,
+        opacity:0
+        },200);
+      setTimeout(function(){
+        $(item).remove();             
+      }, 1000);
   }
   
   PunchList.prototype.drawItems = function(items) {
@@ -84,18 +128,13 @@
     var punchlist_items = $(this.element).find('#punchlist-items');
     punchlist_items.html(htmlPunchListContainer);    
     /* Adding Behaviour */
-    $(this.element).find('.fa-times-circle').click(function(){
+    var self = this;
+    
+    $(this.element).find('.fa-times-circle').click( function(){
           var parentItem = $(this).parent().parent().parent();
-          parentItem.animate({
-            left:"-30%",
-            height:0,
-            opacity:0
-            },200);
-          setTimeout(function(){
-            $(parentItem).remove();             
-          }, 1000);
-        });        
-
+          self.removeItem(parentItem);
+    });
+    
     $(this.element).find('.fa-comment').click(function(){
       var parentItem = $(this).parent().parent().parent();
       $(parentItem).find('.comments').toggleClass('hidden');
@@ -106,68 +145,14 @@
       return new PunchList( this, options );
   }
   
-})( jQuery, window, document );
-
-/*
-(function ( $ ) {
-        
-      $('#add-todo').click(function(){
-        i++;
-        var newId = i;
-        var html_item = [{id:newId}].map(PunchListItemEditing).join('');
-        
-        punchlist_items.append(html_item);
-        
-        $('#input-todo'+newId+'').parent().parent().animate({
-          height:"36px"
-        },200);
-        $('#input-todo'+newId+'').focus();
-        $('#input-todo'+newId+'').enterKey(function(){
-          $(this).trigger('enterEvent');
+  $.fn.enterKey = function (fnc) {
+    return this.each(function () {
+        $(this).keypress(function (ev) {
+            var keycode = (ev.keyCode ? ev.keyCode : ev.which);
+            if (keycode == '13') {
+                fnc.call(this, ev);
+            }
         })
-        
-        $('#input-todo'+newId+'').on('blur enterEvent',function(){
-          var todoTitle = $('#input-todo'+newId+'').val();
-          var todoTitleLength = todoTitle.length;
-      if (todoTitleLength > 0) {
-        $(this).before(todoTitle);
-        $(this).parent().parent().removeClass('editing');
-        $(this).parent().after('<span class="delete-item" title="remove"><i class="fa fa-times-circle"></i></span>');
-        $(this).remove();
-        $('.delete-item').click(function(){
-          var parentItem = $(this).parent();
-          parentItem.animate({
-            left:"-30%",
-            height:0,
-            opacity:0
-          },200);
-          setTimeout(function(){ $(parentItem).remove(); }, 1000);
-        });
-      }
-      else {
-        $('.editing').animate({
-          height:'0px'
-        },200);
-        setTimeout(function(){
-          $('.editing').remove()
-        },400)
-      }
     });
-
-  });
-        
-        return this;
-    };
-    
-      $.fn.enterKey = function (fnc) {
-      return this.each(function () {
-          $(this).keypress(function (ev) {
-              var keycode = (ev.keyCode ? ev.keyCode : ev.which);
-              if (keycode == '13') {
-                  fnc.call(this, ev);
-              }
-          })
-      })
   }
- 
-}( jQuery ));*/
+})( jQuery, window, document );
